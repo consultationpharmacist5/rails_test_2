ddd1e504c35d807b52dd52a96f30f43447e80bf6
Tomoya Morota <consultation.pharmacist5@gmail.com>
2019-04-14 14:52:26 +0900

    controllerにredirect_toメソッド追記

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,5 +10,6 @@ class BlogsController < ApplicationController
 
   def create
     Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    redirect_to "/blogs/new"
   end
 end
